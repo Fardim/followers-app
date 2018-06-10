049f020c57711acfe9fce6dc0501881a4e9fb9ce
unknown <fardim_drmc_9094@yahoo.com>
2018-06-10 18:33:48 +0600

    prepare for heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+//...api end points here
+
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
